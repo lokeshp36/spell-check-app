99240b300ff28eb58e3ddab3bae996a2e45e8115
lokesh p <lokeshpatidar@rarecrew.com>
2022-08-18 11:51:33 +0530

    FIX: highlight the errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
 import { SpellCheckService } from "./spell-check.service";
 
 @Component({
@@ -8,23 +8,37 @@ import { SpellCheckService } from "./spell-check.service";
 })
 export class AppComponent implements OnInit {
   constructor(private spellCheckService: SpellCheckService) {}
+  @ViewChild("oneWithText", null) oneWithText;
+  @ViewChild("oneWithColors", null) oneWithColors;
   title = "spell-check-app";
 
   timeout: any = null;
 
-  ngOnInit(): void {
-    // this.callAPI("sdfsdf");
-  }
+  ngOnInit(): void {}
 
-  checkSpelling(text) {
+  checkSpelling(element) {
+    this.oneWithColors.nativeElement.innerHTML = this.oneWithText.nativeElement.innerHTML;
+    console.log(element);
+    let text = element.textContent;
     clearTimeout(this.timeout);
 
     this.timeout = setTimeout(() => {
       if (text) {
-        this.spellCheckService
-          .checkSpelling(text)
-          .subscribe((res) => console.log(res));
+        this.spellCheckService.checkSpelling(text).subscribe((res: any) => {
+          console.log(res.response.errors[0]);
+          if (res.response.errors.length) {
+            this.highlightText(res.response.errors);
+          }
+        });
       }
     }, 2000);
+  }
+
+  highlightText(errors) {
+    console.log(this.oneWithText);
+    errors.forEach((error) => {
+      let replacementString = "<span class='error'>" + error.bad + "</span>";
+      this.oneWithColors.nativeElement.innerHTML = this.oneWithColors.nativeElement.innerHTML.replace(new RegExp("\\b"+error.bad+"\\b"), replacementString);
+    });
   }
 }
